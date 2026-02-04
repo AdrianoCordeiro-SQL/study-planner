87599b85080ef968679fc1a78603df73ac68428b
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-02-04 07:29:52 -0300

    Cria slice do analytics

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import taskReducer from './slices/taskSlice'
 import themeReducer from './slices/themeSlice';
+import analyticsReducer from './slices/analyticsSlice';
+
 
 export const store = configureStore({
   reducer: {
     tasks: taskReducer,
     theme: themeReducer,
+    analytics: analyticsReducer,
   },
 })
 

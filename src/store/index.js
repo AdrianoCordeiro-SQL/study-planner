a99aa29c180880f95be8cfe914a3aba454367dcb
Adriano Cordeiro <adrianocordeiro99@outlook.com>
2026-02-04 08:47:35 -0300

    cria funções de acesso ao localStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import taskReducer from './slices/taskSlice'
-import themeReducer from './slices/themeSlice';
-import analyticsReducer from './slices/analyticsSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import taskReducer from "./slices/taskSlice";
+import themeReducer from "./slices/themeSlice";
+import analyticsReducer from "./slices/analyticsSlice";
+import { loadState, saveState, throttle } from './localStorage';
 
+const persistedState = loadState();
 
 export const store = configureStore({
   reducer: {
@@ -10,5 +12,18 @@ export const store = configureStore({
     theme: themeReducer,
     analytics: analyticsReducer,
   },
-})
+  devTools: process.env.NODE_ENV !== "production",
+  preloadedState,
+});
 
+const throttleSaveState = throttle((state) => {
+  saveState({
+    tasks: state.tasks,
+    theme: state.theme,
+    analytics: state.analytics,
+  });
+}, 1000);
+
+store.subscribe(() => {
+  throttleSaveState(store.getState());
+});
